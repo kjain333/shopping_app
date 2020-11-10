1e783c0aa9370edeef197aed7969071ffcd535b6
reshma-avvaru <reshmaavvaru@gmail.com>
2020-11-10 17:28:57 +0530

    sign-in with email and pass

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,15 +1,59 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shoppingapp/MainPage.dart';
 import './background.dart';
 import 'package:shoppingapp/Screens/Signup/signup_screen.dart';
 import 'package:shoppingapp/components/already_have_an_account_acheck.dart';
 import 'package:shoppingapp/components/rounded_button.dart';
-import 'package:shoppingapp/components/rounded_input_field.dart';
-import 'package:shoppingapp/components/rounded_password_field.dart';
-class Body extends StatelessWidget {
-  const Body({
-    Key key,
-  }) : super(key: key);
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+class _BodyState extends State<Body> {
+
+  String email, password;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  final passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'password is required'),
+    MinLengthValidator(8, errorText: 'password must be at least 8 digits long'),
+    PatternValidator(r'(?=.*?[#?!@$%^&*-])',
+        errorText: 'passwords must have at least one special character')
+  ]);
+
+  void _signIn({String email,String pass}){
+    _auth.signInWithEmailAndPassword(email: email, password:pass).then((authResult){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MainPage()));
+    }).catchError((err){
+      print(err.code);
+      if(err.code == 'wrong-password'||err.code=='user-not-found'){
+        showCupertinoDialog(context: context, builder: (context){
+          return CupertinoAlertDialog(
+            title: Text(
+                'Incorrect credentials'
+            ),
+            actions: <Widget>[
+              CupertinoDialogAction(child: Text('OK'),
+                onPressed: (){
+                  Navigator.pop(context);
+                },)
+            ],
+          );
+        });
+      }
+    });
+  }
+  void validate() {
+    if (formkey.currentState.validate()) {
+      formkey.currentState.save();
+      _signIn(email: email,pass: password);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,23 +68,67 @@ class Body extends StatelessWidget {
               style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: size.height * 0.03),
-            RoundedInputField(
-              hintText: "Your Email",
-              onChanged: (value) {},
-            ),
-            RoundedPasswordField(
-              onChanged: (value) {},
+            Center(
+              child: Form(
+                key: formkey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          labelText: 'E-mail',
+                        ),
+                        onChanged: (textValue) {
+                          setState(() {
+                            email = textValue;
+                          });
+                        },
+                        validator: EmailValidator(
+                            errorText: 'enter a valid email address'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10.0,
+                          horizontal: 50.0,
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          labelText: 'Password',
+                        ),
+                        onChanged: (textValue) {
+                          setState(() {
+                            password = textValue;
+                          });
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
             RoundedButton(
               text: "LOGIN",
               press: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>MainPage()));
+                validate();
               },
             ),
             SizedBox(height: size.height * 0.03),
             AlreadyHaveAnAccountCheck(
               press: () {
-                Navigator.push(
+                Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
                     builder: (context) {
